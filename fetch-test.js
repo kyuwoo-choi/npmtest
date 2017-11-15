0b85710a7c077ba7fa0dd910c3c0b738aeed08ef
kyuwoo.choi <kyuwoo.choi@nhnent.com>
2017-11-15 20:48:01 +0900

    t2

--- fetch-test.js
+++ fetch-test.js
@@ -1,4 +1,4 @@
-fetch('https://github.com/kyuwoo-choi/npmtest/wiki')
+fetch('https://github.com/kyuwoo-choi/npmtest/wiki', {mode: 'cors'})
     .then(res => console.log(res.text()))
     .catch(error => console.log(error));
 
